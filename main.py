62e094bf3f3224e76e94f1346af42e7ea0b0ce49
Prakhar Gupta <visitprakhar@gmail.com>
2023-09-04 13:05:33 -0500

    made df format for master output

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ import pandas as pd
 from helpers import import_data
 
 data = import_data('./data/')
+# create format for mastersheet
+df_master = pd.DataFrame(columns=['Name', 'Email', 'Discord', 'Year', 'Major', 'Misc'])
+df_format = pd.DataFrame(columns=['Name', 'Email', 'Discord', 'Year', 'Major', 'Misc']) #create format for mastersheet
 
-print(data)
+#misc will contain comma-seperated/dictionary of data from other fields that are not specified
 
+for db in data:
+    df_temp = df_format
+    print(db.head())
+    
 
 
